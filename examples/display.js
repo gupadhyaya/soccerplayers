256d4695db4bfc88a9021775badf00b489c2af7b
Ganesha Upadhyaya <ganeshrvce@gmail.com>
2020-06-24 21:55:14 -0700

    added examples of other contract functions

---
+++ examples/display.js
@@ -0,0 +1,43 @@
+require("dotenv").config();
+const { Harmony } = require("@harmony-js/core");
+const { ChainID, ChainType } = require("@harmony-js/utils");
+const hmy = new Harmony(
+  // let's assume we deploy smart contract to this end-point URL
+  "https://api.s0.b.hmny.io",
+  {
+    chainType: ChainType.Harmony,
+    chainId: ChainID.HmyTestnet,
+  }
+);
+const contractJson = require("../build/contracts/SoccerPlayers.json");
+const contractAddr = "0x16B729874BD8d54A1a3b5d2B0Ff344d3C6604eE0";
+
+const soccerPlayers = hmy.contracts.createContract(
+  contractJson.abi,
+  contractAddr
+);
+// soccerPlayers.wallet.addByPrivateKey(
+//   "1f054c21a0f57ebc402c00e14bd1707ddf45542d4ed9989933dbefc4ea96ca68"
+// );
+
+const options = {
+  gasPrice: process.env.GAS_PRICE,
+  gasLimit: process.env.GAS_LIMIT,
+};
+const instance = soccerPlayers.methods;
+
+async function display() {
+  let total = await instance.totalSupply().call(options);
+  console.log("total players: " + total.toString());
+  for (i = 0; i < total; i++) {
+    let res = await instance.getPlayer(i).call(options);
+    console.log("========== Player info (index: " + i + ")==========");
+    console.log("name", res.playerName);
+    console.log("internalPlayerId", res.internalPlayerId);
+    console.log("sellingPrice", res.sellingPrice);
+    console.log("owner", res.owner);
+  }
+}
+display().then(() => {
+    process.exit(0);
+});
---
+++ examples/others.js
@@ -0,0 +1,50 @@
+require("dotenv").config();
+const { Harmony } = require("@harmony-js/core");
+const { ChainID, ChainType } = require("@harmony-js/utils");
+const hmy = new Harmony(
+  // let's assume we deploy smart contract to this end-point URL
+  "https://api.s0.b.hmny.io",
+  {
+    chainType: ChainType.Harmony,
+    chainId: ChainID.HmyTestnet,
+  }
+);
+const contractJson = require("../build/contracts/SoccerPlayers.json");
+const contractAddr = "0x16B729874BD8d54A1a3b5d2B0Ff344d3C6604eE0";
+
+const soccerPlayers = hmy.contracts.createContract(
+  contractJson.abi,
+  contractAddr
+);
+const addr = "0xF163B63Bc569F39C5D5b399bEE0568339aD7FB13";
+
+const options = {
+  gasPrice: process.env.GAS_PRICE,
+  gasLimit: process.env.GAS_LIMIT,
+};
+const instance = soccerPlayers.methods;
+
+async function test(address) {
+  let balance = await instance.balanceOf(addr).call(options);
+  console.log("balance of " + addr + ": " + balance);
+  const token = 1;
+  let owner = await instance.ownerOf(token).call(options);
+  console.log("owner of token " + token + " is " + owner);
+  let price = await instance.priceOf(token).call(options);
+  console.log("price of token " + token + " is " + price);
+  let transactionCount = await instance.transactionCountOf(token).call(options);
+  console.log("transactions of token " + token + " is " + transactionCount);
+  let tokensOf = await instance.tokensOfOwner(addr).call(options);
+  console.log("tokens of address: " + addr);
+  tokensOf.forEach(token => {
+    console.log(token.toString());
+  });
+  
+}
+test(addr).then(() => {
+    process.exit(0);
+});
+
+
+
+
